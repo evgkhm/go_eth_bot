7eb3d6ef425b3394312cf1e0cd98a4a628788d64
Evgenii <evgkhm@mail.ru>
2022-10-22 21:16:21 +0300

    some commit 5

--- main.go
+++ main.go
@@ -39,8 +39,8 @@ type CryptoResponseGas struct {
 // firstKeyboard первая клавиатура для отображения в ТГ
 var firstKeyboard = tgbotapi.NewInlineKeyboardMarkup(
 	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("📊Цена ETH", "/get_price"),
-		tgbotapi.NewInlineKeyboardButtonData("⛽Цена Gas", "/get_gas"),
+		tgbotapi.NewInlineKeyboardButtonData("📊Цена ETH!", "/get_price"),
+		tgbotapi.NewInlineKeyboardButtonData("⛽Цена Gas!", "/get_gas"),
 	),
 )
 
