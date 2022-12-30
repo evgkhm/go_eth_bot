bf59f1607180b546e24be53893ed0dc0e9ae79bb
Хмелев Евгений <khmelev.ea@kamaz.ru>
2022-12-30 08:14:42 +0300

    refactor v2

--- pkg/telegram/telegram.go
+++ pkg/telegram/telegram.go
@@ -1,10 +1,8 @@
 package telegram
 
 import (
-	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"go_eth_bot/config"
-	"go_eth_bot/internal/entity"
 	"log"
 )
 
@@ -26,25 +24,9 @@ func (u Updates) Run(cfg *config.Config) {
 
 	for update := range u.updates {
 		if update.Message != nil && update.Message.Text == "/start" {
-			//в чат вошел новый пользователь. Поприветствуем его
-			str := fmt.Sprintf(`Привет %s! Этот бот показывает стоимость эфира, газа и текущий баланс.
-Для проверки баланса введите адрес кошелька`, update.Message.From.FirstName)
-			SendTgMess(update.Message.Chat.ID, str, u.bot, First)
+			Greeting(update.CallbackQuery.Message.Chat.ID, update.Message.From.FirstName, u.bot)
 		} else if update.Message != nil {
-			//если получили обычное сообщение сообщение от пользователя в ТГ
-			var newResp entity.CryptoUserData
-			newResp.Address = update.Message.Text
-			if IsValidAddress(newResp.Address) {
-				//получаем ID пользователя
-				ChatID := update.Message.Chat.ID
-				usersList[ChatID] = newResp.Address
-				str := "Адрес получен. Выберете действие"
-				SendTgMess(update.Message.Chat.ID, str, u.bot, Second)
-			} else {
-				newResp.Address = ""
-				str := "Введите ETH адрес"
-				SendTgMess(update.Message.Chat.ID, str, u.bot, First)
-			}
+			PutAddToMap(update.CallbackQuery.Message.Chat.ID, usersList, update.Message.Text, u.bot)
 		}
 
 		//если получили нажатие кнопки
@@ -63,13 +45,7 @@ func (u Updates) Run(cfg *config.Config) {
 				GetEthGas(update.CallbackQuery.Message.Chat.ID, usersList, cfg, u.bot)
 
 			case "/change_addr":
-				//получаем ID пользователя
-				ChatID := update.CallbackQuery.Message.Chat.ID
-				//удаляем из мапы пользователя
-				delete(usersList, ChatID)
-				str := "Введите ETH адрес"
-				SendTgMess(update.CallbackQuery.Message.Chat.ID, str, u.bot, First)
-
+				ChangeAddress(update.CallbackQuery.Message.Chat.ID, usersList, u.bot)
 			}
 		}
 	}
